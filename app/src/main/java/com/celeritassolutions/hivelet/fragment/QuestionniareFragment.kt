72fc56ea5cc1c637b8da480bbe8391635467db98
asif <asif.jawaid@celeritas-solutions.com>
2019-02-20 13:50:34 +0500

    initial commit

--- app/src/main/java/com/celeritassolutions/hivelet/fragment/QuestionniareFragment.kt
+++ app/src/main/java/com/celeritassolutions/hivelet/fragment/QuestionniareFragment.kt
@@ -246,7 +246,7 @@ class QuestionniareFragment : Fragment(){
     }
 
     fun donothing(){
-        Toast.makeText(context.applicationContext,"Do Nothing",Toast.LENGTH_SHORT).show()
+        Toast.makeText(context!!.applicationContext,"Do Nothing",Toast.LENGTH_SHORT).show()
     }
 
     private fun showDialog(txtTitle:String, queText: String) {
